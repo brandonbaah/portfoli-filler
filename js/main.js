94b47bfc57597159250af49effa154f655d08c5f
Brandon Baah <brandonbaah@gmail.com>
2019-08-19 15:48:07 -0400

    added-cool-snippet

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+$(function(){
+                $(".likes").typed({
+                    strings: ["EARLY MORNING GYM RAT", "BASKETBALL COMMENTATOR", "FRONT END NINJA", "PHP WARRIOR", "RUBY REBEL"],
+                    typeSpeed: 0,
+                    showCursor: false,
+                    loop: true,
+                    loopCount: false,
+                    backDelay: 1000
+                });
+            });
+});
